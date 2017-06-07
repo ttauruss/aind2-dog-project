3271790babf7173b5424faa80459d4df4221c494
Sergey Olikh <ttauruss@gmail.com>
2017-06-07 06:15:01 +0000

    augmentation files

---
+++ gen_bottleneck.py
@@ -0,0 +1,39 @@
+
+from keras.applications.resnet50 import preprocess_input
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+import numpy as np
+import glob
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in img_paths]
+    return np.vstack(list_of_tensors)
+
+data_types = ['train', 'test', 'valid']
+predictions = []
+
+model = ResNet50(include_top=False)
+
+for dt in data_types:
+    img_paths = glob.glob("dogImages/" + dt + "/*/*")
+    # img_paths = glob.glob("dogImages_copy/" + dt + "/*/*")
+    # img_paths = glob.glob("dogImages_copy/" + dt + "/001.Affenpinscher/*")
+
+    img_input = preprocess_input(paths_to_tensor(img_paths))
+
+    print(img_input.shape)
+
+    res = model.predict(img_input)
+    predictions.append(res)
+
+np.savez('test', train=predictions[0], test=predictions[1], valid=predictions[2])
--- image_generator.py
+++ image_generator.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from os.path import split
 from glob import glob
+from tqdm import tqdm
+import numpy as np
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 datagen = ImageDataGenerator(
         rotation_range=40,
@@ -13,21 +17,22 @@ datagen = ImageDataGenerator(
         horizontal_flip=True,
         fill_mode='nearest')
 
-files = glob("dogImages/test/*/*")
+files = glob("dogImages/train/*/*")
 
-for path in files:
-    print(path)
+for path in tqdm(files):
+    # print('{}/{} - {}'.format(idx+1, len(files), path))
     d, f = split(path)
     prefix = f.split('.')[0]
-    print(prefix)
-    img = load_img(path)
+    # print(prefix)
+    img = load_img(path, target_size=(224, 224))
     x = img_to_array(img)
-    x = x.reshape((1,) + x.shape)
+    # x = x.reshape((1,) + x.shape)
+    x = np.expand_dims(x, axis=0)
 
     i = 0
     for batch in datagen.flow(x, batch_size=1, save_to_dir=d, save_prefix=prefix, save_format='jpg'):
         i += 1
-        if i > 2:
+        if i > 0:
             break
 
 # gen_files = glob('preview/*')
